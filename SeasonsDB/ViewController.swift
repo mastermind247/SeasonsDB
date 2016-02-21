246f366f29e596f60c56348f6f7796808f729a2a
Parth Adroja <parth2cool2008@gmail.com>
2016-02-21 11:43:00 +0530

    Initial Commit

---
+++ SeasonsDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeasonsDB
+//
+//  Created by Parth Adroja on 2/21/16.
+//  Copyright © 2016 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
